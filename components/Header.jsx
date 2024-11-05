eb49fe382641f4031bfb86f5a78da425d8307dd5
Timm-Osorio <162040931+Timm-Osorio@users.noreply.github.com>
2024-11-05 15:37:59 +0800

    header better

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,28 +2,9 @@ import React from 'react';
 import { View, Text, StyleSheet, TextInput, TouchableOpacity, Image } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { Link } from 'expo-router';
-import { useState } from 'react';
-import Animated, { useSharedValue, useAnimatedStyle, withTiming, Easing } from 'react-native-reanimated';
+
 
 const Header= () => {
-  const [isFocused, setIsFocused] = useState(false);
-  const animationValue = useSharedValue(0);
-  const [containerWidth, setContainerWidth] = useState(0);
-
-  const handleLayout = (event) => {
-    setContainerWidth(event.nativeEvent.layout.width);
-  };
-
-  const animatedStyle = useAnimatedStyle(() => {
-
-    const targetWidth = isFocused ? containerWidth * 0.8 : containerWidth * 0.2;
-    return {
-      width: withTiming(targetWidth, {
-        duration: 350,
-        easing: Easing.bezier(0.25, 0.5, 0.7, 0.94),
-      }),
-    };
-  });
 
   return (
     <View 
@@ -41,24 +22,20 @@ const Header= () => {
       </View>
 
       {/* Search Bar and Button */}
-      <View className="flex-row items-center ml-8 flex gap-4 sm:flex-auto justify-end" onLayout={handleLayout}>
-        <View className="relative h-10 flex-row items-center  bg-slate-200 rounded-md pl-3 py-1">
-          <Animated.View className="mr-2" style={animatedStyle}>
+      <View className="flex-row items-center ml-8 flex gap-4 sm:flex-auto justify-end " >
+        <View className="group relative h-10 flex-row items-center  bg-slate-200 rounded-md pl-3 py-1 w-56 lg:hover:w-[60vw] md:hover:w-[50vw] hover:w-[40vw] duration-300 transition-all">
             <TextInput
               placeholder="Search..."
               placeholderTextColor="#888"
               className="flex-1 text-black"
-              onFocus={() => setIsFocused(true)}
-              onBlur={() => setIsFocused(false)}
-              style={{ outline: 'none' }}
               ellipsizeMode="tail"
+              style={{outlineStyle: 'none' }}
               numberOfLines={1}
               textAlign='left'
             />
-          </Animated.View>
-
-          <TouchableOpacity className="w-10 h-10 px-2 justify-center flex self-center text-left bg-slate-200 hover:bg-slate-300 duration-300 rounded-r-md">
-              <Ionicons name="search" size={20} color={isFocused ? "#9800FF" : "#555"} />
+     
+          <TouchableOpacity className="w-10 h-10 px-2 justify-center flex self-center text-left bg-slate-200 hover:bg-slate-300 group-hover:bg-primary-color duration-300 transition-all rounded-r-md ">
+              <Ionicons name="search" size={20} className='group-hover:color-black color-primary-color duration-300 transition-all'/>
           </TouchableOpacity>
           
         </View>
