e0526a55f8d41fe8afb3a1ae86f5593a2bdfb257
Jhonjhorie <74289488+Jhonjhorie@users.noreply.github.com>
2024-11-11 14:07:19 +0800

    before import

--- app/(pages)/account/avatar.jsx
+++ app/(pages)/account/avatar.jsx
@@ -4,12 +4,79 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { View, StyleSheet } from "react-native";
 
-const Cube = () => {
+const CharacterFace = () => {
   return (
-    <mesh rotation={[45, 45, 0]}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={"orange"} />
-    </mesh>
+    <>
+      {/* Head */}
+      <mesh position={[0, 1.5, 0]}>
+        <sphereGeometry args={[0.5, 32, 32]} />
+        <meshStandardMaterial color={"#ffcc99"} />
+      </mesh>
+
+      {/* Eyes */}
+      <mesh position={[-0.15, 1.65, 0.25]}>
+        <sphereGeometry args={[0.1, 32, 32]} />
+        <meshStandardMaterial color={"white"} />
+      </mesh>
+      <mesh position={[0.15, 1.65, 0.25]}>
+        <sphereGeometry args={[0.1, 32, 32]} />
+        <meshStandardMaterial color={"white"} />
+      </mesh>
+
+      {/* Pupils */}
+      <mesh position={[-0.15, 1.65, 0.35]}>
+        <sphereGeometry args={[0.05, 32, 32]} />
+        <meshStandardMaterial color={"black"} />
+      </mesh>
+      <mesh position={[0.15, 1.65, 0.35]}>
+        <sphereGeometry args={[0.05, 32, 32]} />
+        <meshStandardMaterial color={"black"} />
+      </mesh>
+
+      {/* Mouth */}
+      <mesh position={[0, 1.4, 0.25]}>
+        <planeGeometry args={[0.3, 0.1]} />
+        <meshStandardMaterial color={"red"} />
+      </mesh>
+
+      {/* Nose */}
+      <mesh position={[0, 1.5, 0.4]}>
+        <coneGeometry args={[0.05, 0.1, 32]} />
+        <meshStandardMaterial color={"#ffcc99"} />
+      </mesh>
+    </>
+  );
+};
+
+const CharacterBody = () => {
+  return (
+    <>
+      {/* Body */}
+      <mesh position={[0, 0.5, 0]}>
+        <cylinderGeometry args={[0.4, 0.4, 1, 32]} />
+        <meshStandardMaterial color={"#007BFF"} /> {/* Shirt color */}
+      </mesh>
+
+      {/* Arms */}
+      <mesh position={[-0.6, 0.5, 0]}>
+        <cylinderGeometry args={[0.1, 0.1, 1, 32]} />
+        <meshStandardMaterial color={"#ffcc99"} />
+      </mesh>
+      <mesh position={[0.6, 0.5, 0]}>
+        <cylinderGeometry args={[0.1, 0.1, 1, 32]} />
+        <meshStandardMaterial color={"#ffcc99"} />
+      </mesh>
+
+      {/* Legs */}
+      <mesh position={[-0.25, -0.5, 0]}>
+        <cylinderGeometry args={[0.15, 0.15, 1, 32]} />
+        <meshStandardMaterial color={"#6c757d"} /> {/* Pants color */}
+      </mesh>
+      <mesh position={[0.25, -0.5, 0]}>
+        <cylinderGeometry args={[0.15, 0.15, 1, 32]} />
+        <meshStandardMaterial color={"#6c757d"} />
+      </mesh>
+    </>
   );
 };
 
@@ -19,7 +86,8 @@ const BasicDisplay = () => {
       <Canvas style={styles.canvas}>
         <ambientLight intensity={0.5} />
         <directionalLight position={[5, 5, 5]} intensity={1} />
-        <Cube />
+        <CharacterFace />
+        <CharacterBody />
         <OrbitControls enableZoom={true} />
       </Canvas>
     </View>
