ac3b7467b8ad3233f4718c20b643a151ffccfe04
Inzagh1 <corporal.paolo.11162002@gmail.com>
2024-11-14 23:48:40 +0800

    sidebaroks

---
+++ components/order/orderCard.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { FlatList, Image, Text, View } from 'react-native';
+import { Link } from 'expo-router';
+
+const OrderCard = ({ orders  }) => {
+const sorted = orders.slice(0, 2);
+
+  return (
+    <View className='overflow-hidden'
+    style={{ width: '63%' , height:'77%',}}
+        >
+      <FlatList
+        scrollEnabled={false}
+        data={sorted}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <Link href='/' 
+          className='flex flex-row bg-gray-200 rounded-md group mt-2 py-1 px-2' style={{ width: '100%', height: '100%' }}>
+            <Image
+              source={typeof item.url === 'string' ? { uri: item.url } : item.url}
+              className='bg-slate-50 rounded-lg'
+              style={{ width: 80, height: 80 }}
+              resizeMode="contain"
+            />
+            <View className='ml-2 p-1'>
+              <Text className='text-secondary-color text-xl'>{item.product}</Text>
+              <Text className='text-secondary-color'>{item.shop}</Text>
+              <Text className='text-primary-color font-bold'>TO RECEIVE</Text>
+            </View>
+          </Link>
+        )}
+   
+        ListEmptyComponent={<Text>No orders available</Text>} 
+      />
+    </View>
+  );
+};
+
+export default OrderCard;
